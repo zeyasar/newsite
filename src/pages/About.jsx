6143264b876f4ab5db9e504c584b7523d8538010
zeynep <zynpysr67@gmail.com>
2023-02-03 13:57:10 +0300

    9

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,10 +4,14 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import milestone from "../assets/mılestoneProject.gif";
+import portfolio from "../assets/portfolio.gif";
+import movieApp from "../assets/movıeApp.gif";
+
 
 
 export default function About() {
@@ -38,7 +42,7 @@ export default function About() {
           </Typography>
           <Typography variant="body1" sx={{ mt: 5 }}>
             <Link
-              href="https://github.com/zeyasar/milestone-project"
+              href="https://github.com/zeyasar/portfolio-project"
               target="_blank"
               rel="noopener"
               underline="none"
@@ -46,37 +50,124 @@ export default function About() {
               Github
             </Link>
           </Typography>
-          <iframe></iframe>
         </Container>
 
-        <Container>
-        <Card sx={{ maxWidth: 345 }}>
+        <Box
+          xs={{ d: "flex" }}
+          display="flex"
+          alignItems="center"
+          justifyContent="space-evenly"
+          flexWrap="wrap"
+        >
+          <Card sx={{ width: 345, m: 5, maxHeight: 700 }}>
             <CardMedia
               component="img"
               alt="green iguana"
               height="140"
-              image="/static/images/cards/contemplative-reptile.jpg"
+              image={portfolio}
             />
-            <CardActions>
-              <Button size="small"><Link
-              href="https://github.com/zeyasar/milestone-project"
-              target="_blank"
-              rel="noopener"
-              underline="none"
+            <CardActions
+              sx={{ display: "flex", justifyContent: "space-evenly" }}
             >
-              Github 
-            </Link></Button>
-              <Button size="small"><Link
-              href="https://github.com/zeyasar/milestone-project"
-              target="_blank"
-              rel="noopener"
-              underline="none"
-            >
-              Demo
-            </Link></Button>
+              <Button size="small">
+                <Link
+                  href="https://github.com/zeyasar/portfolio"
+                  target="_blank"
+                  rel="noopener"
+                  underline="none"
+                  color={"black"}
+                  variant="subtitle1"
+                >
+                  Github
+                </Link>
+              </Button>
+              <Button size="small">
+                <Link
+                  href="https://devzeynep-portfolio.vercel.app/"
+                  target="_blank"
+                  rel="noopener"
+                  underline="none"
+                  color={"black"}
+                  variant="subtitle1"
+                >
+                  Demo
+                </Link>
+              </Button>
             </CardActions>
           </Card>
-        </Container>
+          <Card sx={{ width: 345, m: 5, maxHeight: 700 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image={milestone}
+            />
+            <CardActions
+              sx={{ display: "flex", justifyContent: "space-evenly" }}
+            >
+              <Button size="small">
+                <Link
+                  href="https://github.com/zeyasar/milestone-project"
+                  target="_blank"
+                  rel="noopener"
+                  underline="none"
+                  color={"black"}
+                  variant="subtitle1"
+                >
+                  Github
+                </Link>
+              </Button>
+              <Button size="small">
+                <Link
+                  href="https://milestone-project.netlify.app/"
+                  target="_blank"
+                  rel="noopener"
+                  underline="none"
+                  color={"black"}
+                  variant="subtitle1"
+                >
+                  Demo
+                </Link>
+              </Button>
+            </CardActions>
+          </Card>
+          <Card sx={{ width: 345, m: 5, maxHeight: 700 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image={movieApp}
+            />
+            <CardActions
+              sx={{ display: "flex", justifyContent: "space-evenly" }}
+            >
+              <Button size="small">
+                <Link
+                  href="https://github.com/zeyasar/Movie_App_Project_with_Firebase"
+                  target="_blank"
+                  rel="noopener"
+                  underline="none"
+                  color={"black"}
+                  variant="subtitle1"
+                >
+                  Github
+                </Link>
+              </Button>
+              <Button size="small">
+                <Link
+                  href="https://movie-project-firebase.netlify.app"
+                  target="_blank"
+                  rel="noopener"
+                  underline="none"
+                  color={"black"}
+                  variant="subtitle1"
+                >
+                  Demo
+                </Link>
+              </Button>
+            </CardActions>
+          </Card>
+        </Box>
       </Box>
     </>
   );
